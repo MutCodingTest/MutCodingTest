7ead386e9e60cfb815fa04363ff79d5114a1828a
이건회 <rg970604@naver.com>
2022-07-03 02:54:26 +0900

    Create 백준11724.py

---
+++ 2주차/이건회/백준11724.py
@@ -0,0 +1,32 @@
+n,m=map(int,input().split())
+parent=[0] * (n+1)
+graphs=[]
+
+for i in range(1,n+1):
+  parent[i]=i
+
+for _ in range(m):
+  u,v=map(int,input().split())
+  graphs.append((u,v))
+
+def find_parent(parent,x):
+  if parent[x] != x:
+    parent[x]=find_parent(parent,parent[x])
+  return parent[x]
+
+def union_parent(parent,a,b):
+  a= find_parent(parent,a)
+  b= find_parent(parent,b)
+  if a<b:
+    parent[b]=a
+  else:
+    parent[a]=b
+
+for graph in graphs:
+  a,b=graph
+  if find_parent(parent,a) != find_parent(parent,b):
+    union_parent(parent,a,b)
+
+parent_set=set(parent[1:])
+parent_list=list(parent_set)
+print(len(parent_list))
