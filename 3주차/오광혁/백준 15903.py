14bdfe7c39bea2db7f94a374117b9905c5979827
panghyuk <okh9712@gmail.com>
2022-08-03 15:44:57 +0900

    6주차 풀이

--- 3주차/오광혁/백준 15903.py
+++ 3주차/오광혁/백준 15903.py
@@ -1,6 +1,17 @@
 # 키드 합체 놀이/그리디
 
+''' Sol.1 '''
 import sys
 
 input = sys.stdin.readline
 n,m = map(int,input().split())
+
+card = list(map(int,input().split()))
+
+for _ in range(m):
+    card.sort()
+    tmp = card[0] + card[1]
+    card[0] = tmp
+    card[1] = tmp
+
+print(sum(card))
